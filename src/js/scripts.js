5441e0854b3774ed6baf4f5c7d978c3af94ab2f4
Tekla <davidannatekla@gmail.com>
2021-02-06 10:11:21 +0100

    MZ-04 add validation to input

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 
 let list = document.getElementById("list");
-
+let task = document.getElementById("task");
 //function for deleting an item
 function deleteElement(element) {
     element.addEventListener("click", event => {
@@ -9,16 +9,26 @@ function deleteElement(element) {
 }
 //creat list item by clicking on create button and deleting
 document.getElementById("createBtn").addEventListener("click", function (){
-    let listItem = document.createElement("li");
-    list.append(listItem);
-    listItem.innerText = document.getElementById("task").value;
+    if (task.value.length < 6){
+        alert("minimum 6 karakterből álljon a task-od!");
+    } else {
+        let listItem = document.createElement("li");
+        list.append(listItem);
+        listItem.innerText = task.value;
 
-    let deleteBtn = document.createElement("button");
-    listItem.append(deleteBtn);
-    deleteBtn.innerText = "töröl";
+        let deleteBtn = document.createElement("button");
+        listItem.append(deleteBtn);
+        deleteBtn.innerText = "töröl";
 
-    deleteElement(deleteBtn);
-
+        deleteElement(deleteBtn);
+    }
 })
 
-
+//validation of input
+task.addEventListener("keydown", event => {
+    if (task.value.length < 6) {
+        task.setAttribute("style", "background-color: red")
+    } else if (task.value.length > 6) {
+        task.setAttribute("style", "background-color: green")
+    }
+});
