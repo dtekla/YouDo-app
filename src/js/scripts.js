52909353f868dba4bcd8b2f847ee7ae6634db358
Tekla <davidannatekla@gmail.com>
2021-02-06 09:53:13 +0100

    MZ-03 add delete btn

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,24 @@
 
 let list = document.getElementById("list");
-//creat lis item by clicking on create button
+
+//function for deleting an item
+function deleteElement(element) {
+    element.addEventListener("click", event => {
+        event.target.parentElement.remove();
+    })
+}
+//creat list item by clicking on create button and deleting
 document.getElementById("createBtn").addEventListener("click", function (){
     let listItem = document.createElement("li");
     list.append(listItem);
     listItem.innerText = document.getElementById("task").value;
-    })
+
+    let deleteBtn = document.createElement("button");
+    listItem.append(deleteBtn);
+    deleteBtn.innerText = "töröl";
+
+    deleteElement(deleteBtn);
+
+})
 
 
