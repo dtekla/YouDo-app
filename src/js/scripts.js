92473202b6e595c16faf3e35c1d7ce5925b8fbd1
Tekla <davidannatekla@gmail.com>
2021-02-06 09:46:49 +0100

    MZ-02 make create button working

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,10 @@
 
+let list = document.getElementById("list");
+//creat lis item by clicking on create button
+document.getElementById("createBtn").addEventListener("click", function (){
+    let listItem = document.createElement("li");
+    list.append(listItem);
+    listItem.innerText = document.getElementById("task").value;
+    })
+
 
